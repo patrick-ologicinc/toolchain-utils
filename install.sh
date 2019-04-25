3574e2b83b04973106c1dd31343fa0756bf59e41
Patrick Lloyd <patrick@lloyd.sh>
2019-04-24 20:42:57 -0700

    added install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+sudo apt-get install build-essential clang bison flex libreadline-dev \
+    gawk tcl-dev libffi-dev git mercurial graphviz   \
+    xdot pkg-config python python3 libftdi-dev
+
+mkdir icestorm-build
+cd icestorm-build
+
+git clone https://github.com/cliffordwolf/icestorm.git icestorm
+cd icestorm
+make -j$(nproc)
+sudo make install
+cd ..
+
+git clone https://github.com/cseed/arachne-pnr.git arachne-pnr
+cd arachne-pnr
+make -j$(nproc)
+sudo make install
+cd ..
+
+git clone https://github.com/cliffordwolf/yosys.git yosys
+cd yosys
+make -j$(nproc)
+sudo make install
+cd ..
